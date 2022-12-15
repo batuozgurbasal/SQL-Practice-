e73d1f15d6eb0043c83680d22baabd3de4ee2f4c
Batuhan Ozgur Basal <101004435+batuozgurbasal@users.noreply.github.com>
2022-12-15 14:48:02 +0000

    Update sql_1.sql

--- sql/sql_1.sql
+++ sql/sql_1.sql
@@ -1,1 +1,28 @@
+/*
+Employee and Manager Salaries
 
+Find employees who are earning more than their managers. Output the employee's first name along with the corresponding salary.
+
+<employee> table
+Preview
+
+id: int
+first_name: varchar
+last_name: varchar
+age: int
+sex: varchar
+employee_title: varchar
+department: varchar
+salary: int
+target: int
+bonus: int
+email: varchar
+city: varchar
+address: varchar
+manager_id: int
+*/
+
+select b.first_name ,b.salary  from employee a
+inner join employee b
+on a.id = b.manager_id
+where a.salary < b.salary
