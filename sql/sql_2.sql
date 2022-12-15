c0d6a708610ca7318b903012b0a90687b73902d1
Batuhan Ozgur Basal <101004435+batuozgurbasal@users.noreply.github.com>
2022-12-15 14:49:52 +0000

    Update sql_2.sql

--- sql/sql_2.sql
+++ sql/sql_2.sql
@@ -1,1 +1,29 @@
+/*
+https://platform.stratascratch.com/coding/10060-top-cool-votes?code_type=1
 
+Top Cool Votes
+
+Find the review_text that received the highest number of  'cool' votes.
+Output the business name along with the review text with the highest numbef of 'cool' votes.
+
+<yelp_reviews> table
+
+business_name: varchar
+review_id: varchar
+user_id: varchar
+stars: varchar
+review_date: datetime
+review_text: varchar
+funny: int
+useful: int
+cool: int
+*/
+
+with tbl as(
+select distinct business_name, review_text, cool, dense_rank() over(order by cool desc) as cool_ranking
+from yelp_reviews
+order by cool desc)
+
+select business_name, review_text
+from tbl
+where cool_ranking = 1
