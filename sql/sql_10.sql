99f92f4dc0d5e9a861f85c716404aa4d77c1c3bb
Batuhan Ozgur Basal <101004435+batuozgurbasal@users.noreply.github.com>
2022-12-15 15:05:51 +0000

    Create sql_10.sql

---
+++ sql/sql_10.sql
@@ -0,0 +1,33 @@
+/*
+https://platform.stratascratch.com/coding/9728-inspections-that-resulted-in-violations?code_type=1
+Number of violations
+
+You're given a dataset of health inspections. Count the number of violation in an inspection in 'Roxanne Cafe' for each year. If an inspection resulted in a violation, 
+there will be a value in the 'violation_id' column. Output the number of violations by year in ascending order.
+
+<sf_restaurant_health_violations> table
+
+business_id: int
+business_name: varchar
+business_address: varchar
+business_city: varchar
+business_state: varchar
+business_postal_code: float
+business_latitude: float
+business_longitude: float
+business_location: varchar
+business_phone_number: float
+inspection_id: varchar
+inspection_date: datetime
+inspection_score: float
+inspection_type: varchar
+violation_id: varchar
+violation_description: varchar
+risk_category: varchar
+
+*/
+
+select date_part('year',inspection_date) as year, count(violation_id) as n_inspections
+from sf_restaurant_health_violations
+where business_name = 'Roxanne Cafe'
+group by year 
